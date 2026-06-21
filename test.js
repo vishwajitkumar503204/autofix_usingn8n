c3bc19f8d6d31e82acf19580c624be41920d5f68
vishwajit kumar <vishwajitkumar503204@gmail.com>
2026-06-21 22:13:53 +0530

    first

---
+++ test.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+const server = app.listen(5000, () => {
+  console.log('Server started, running smoke test...');
+
+  // Send a GET request to the server
+  http.get('http://localhost:5000', (res) => {
+    console.log(`Status: ${res.statusCode}`);
+    if (res.statusCode === 200) {
+      console.log('Smoke test passed!');
+      server.close();
+      process.exit(0);
+    } else {
+      server.close();
+      process.exit(1);
+    }
+  }).on('error', (err) => {
+    console.error('Request failed:', err.message);
+    server.close();
+    process.exit(1);
+  });
+});
